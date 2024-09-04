ca1c414dcf4f75fa49e1887867e63ec388cee0ba
Joshua Studt <josh.studt@underdogfantasy.com>
2024-09-04 16:18:19 -0400

    whitespacing

--- DuoSecurity.Auth.Http/Internal/DuoApiHelper.cs
+++ DuoSecurity.Auth.Http/Internal/DuoApiHelper.cs
@@ -44,97 +44,97 @@ internal static class DuoApiHelper
 {
     public static string CanonicalizeParams(IEnumerable<KeyValuePair<string, string>> parameters)
     {
-            var ret = new List<string>();
-            foreach (var pair in parameters)
-            {
-                string p = string.Format("{0}={1}",
-                                         HttpUtility.UrlEncode(pair.Key),
-                                         HttpUtility.UrlEncode(pair.Value));
-                // Signatures require upper-case hex digits.
-                p = Regex.Replace(p,
-                                  "(%[0-9A-Fa-f][0-9A-Fa-f])",
-                                  c => c.Value.ToUpperInvariant());
-                // Escape only the expected characters.
-                p = Regex.Replace(p,
-                                  "([!'()*])",
-                                  c => "%" + Convert.ToByte(c.Value[0]).ToString("X"));
-                p = p.Replace("%7E", "~");
-                // UrlEncode converts space (" ") to "+". The
-                // signature algorithm requires "%20" instead. Actual
-                // + has already been replaced with %2B.
-                p = p.Replace("+", "%20");
-                ret.Add(p);
-            }
-            ret.Sort(StringComparer.Ordinal);
-            return string.Join("&", ret.ToArray());
+        var ret = new List<string>();
+        foreach (var pair in parameters)
+        {
+            string p = string.Format("{0}={1}",
+                                     HttpUtility.UrlEncode(pair.Key),
+                                     HttpUtility.UrlEncode(pair.Value));
+            // Signatures require upper-case hex digits.
+            p = Regex.Replace(p,
+                              "(%[0-9A-Fa-f][0-9A-Fa-f])",
+                              c => c.Value.ToUpperInvariant());
+            // Escape only the expected characters.
+            p = Regex.Replace(p,
+                              "([!'()*])",
+                              c => "%" + Convert.ToByte(c.Value[0]).ToString("X"));
+            p = p.Replace("%7E", "~");
+            // UrlEncode converts space (" ") to "+". The
+            // signature algorithm requires "%20" instead. Actual
+            // + has already been replaced with %2B.
+            p = p.Replace("+", "%20");
+            ret.Add(p);
         }
+        ret.Sort(StringComparer.Ordinal);
+        return string.Join("&", ret.ToArray());
+    }
 
     public static string CanonicalizeRequest(string method, string host, string endpoint, string canonParams, string date)
     {
-            var lines = new string[]
-            {
-                date,
-                method.ToUpperInvariant(),
-                host.ToLower(),
-                endpoint,
-                canonParams
-            };
+        var lines = new string[]
+        {
+            date,
+            method.ToUpperInvariant(),
+            host.ToLower(),
+            endpoint,
+            canonParams
+        };
 
-            return string.Join("\n", lines);
-        }
+        return string.Join("\n", lines);
+    }
 
     public static string HmacSign(string secretKey, string data)
     {
-            byte[] key_bytes = Encoding.ASCII.GetBytes(secretKey);
-            HMACSHA1 hmac = new HMACSHA1(key_bytes);
+        byte[] key_bytes = Encoding.ASCII.GetBytes(secretKey);
+        HMACSHA1 hmac = new HMACSHA1(key_bytes);
 
-            byte[] data_bytes = Encoding.ASCII.GetBytes(data);
-            hmac.ComputeHash(data_bytes);
+        byte[] data_bytes = Encoding.ASCII.GetBytes(data);
+        hmac.ComputeHash(data_bytes);
 
-            string hex = BitConverter.ToString(hmac.Hash);
-            return hex.Replace("-", "").ToLower();
-        }
+        string hex = BitConverter.ToString(hmac.Hash);
+        return hex.Replace("-", "").ToLower();
+    }
 
     public static string Encode64(string plaintext)
     {
-            byte[] plaintext_bytes = Encoding.ASCII.GetBytes(plaintext);
-            string encoded = Convert.ToBase64String(plaintext_bytes);
-            return encoded;
-        }
+        byte[] plaintext_bytes = Encoding.ASCII.GetBytes(plaintext);
+        string encoded = Convert.ToBase64String(plaintext_bytes);
+        return encoded;
+    }
 
     public static string DateToRFC822(DateTime date)
     {
-            // Can't use the "zzzz" format because it adds a ":"
-            // between the offset's hours and minutes.
-            string date_string = date.ToString(
-                "ddd, dd MMM yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+        // Can't use the "zzzz" format because it adds a ":"
+        // between the offset's hours and minutes.
+        string date_string = date.ToString(
+            "ddd, dd MMM yyyy HH:mm:ss", CultureInfo.InvariantCulture);
 
-            var offset = TimeZoneInfo.Local.GetUtcOffset(date);
-            string zone;
+        var offset = TimeZoneInfo.Local.GetUtcOffset(date);
+        string zone;
 
-            // + or -, then 0-pad, then offset, then more 0-padding.
-            if (offset.TotalHours < 0)
-            {
-                zone = "-";
-            }
-            else
-            {
-                zone = "+";
-            }
-
-            var offsetHours = offset.Hours;
-            if (offsetHours < 0)
-                offsetHours *= -1;
-
-            zone += offsetHours.ToString(CultureInfo.InvariantCulture).PadLeft(2, '0');
-
-            var offsetMinutes = offset.Minutes;
-            if (offsetMinutes < 0)
-                offsetMinutes *= -1;
-
-            zone += offsetMinutes.ToString(CultureInfo.InvariantCulture).PadLeft(2, '0');
-
-            date_string += " " + zone;
-            return date_string;
+        // + or -, then 0-pad, then offset, then more 0-padding.
+        if (offset.TotalHours < 0)
+        {
+            zone = "-";
         }
+        else
+        {
+            zone = "+";
+        }
+
+        var offsetHours = offset.Hours;
+        if (offsetHours < 0)
+            offsetHours *= -1;
+
+        zone += offsetHours.ToString(CultureInfo.InvariantCulture).PadLeft(2, '0');
+
+        var offsetMinutes = offset.Minutes;
+        if (offsetMinutes < 0)
+            offsetMinutes *= -1;
+
+        zone += offsetMinutes.ToString(CultureInfo.InvariantCulture).PadLeft(2, '0');
+
+        date_string += " " + zone;
+        return date_string;
+    }
 }
